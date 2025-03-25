80feec43ab82a048c933822a23a454f56c816b27
Sanjay Ghosh <nerdsubhankar2006@gmail.com>
2025-03-25 19:49:35 +0530

    Add Lombok And H2 support

--- src/main/java/org/acme/App.java
+++ src/main/java/org/acme/App.java
@@ -3,11 +3,19 @@ package org.acme;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @QuarkusMain
+@Slf4j
 public class App {
+
+	private static final Logger log = LoggerFactory.getLogger(App.class);
 
 	public static void main(String [] args) {
 		System.out.println("Quarkus Application");
+		log.info("SLF4J LOGGING --- Quarkus Application --- ");
 		Quarkus.run(args);
 	}
 }
